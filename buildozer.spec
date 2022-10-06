274eba18de148f6430893b49797d75e75e3afba1
remo619 <sidseeker21@gmail.com>
2022-10-06 12:49:24 +0530

    added exit button and presplash image

--- buildozer.spec
+++ buildozer.spec
@@ -46,10 +46,10 @@ requirements = kivy,kivymd,python3==3.9.0,numpy,opencv,python2,Pillow
 #garden_requirements =
 
 # (str) Presplash of the application
-#presplash.filename = %(source.dir)s/data/presplash.png
+presplash.filename = %(source.dir)s/presplash.png
 
 # (str) Icon of the application
-#icon.filename = %(source.dir)s/data/icon.png
+icon.filename = %(source.dir)s/presplash.png
 
 # (str) Supported orientation (one of landscape, portrait or all)
 orientation = portrait
--- main.py
+++ main.py
@@ -19,30 +19,28 @@ if platform=="android":
     ])
 
 class AndroidCam(Camera):
-    camera_resolution = (640, 480)
-    #counter = 0
+    camera_resolution = (640,480)
 
     def _camera_loaded(self, *largs):
         self.texture = Texture.create(size=np.flip(self.camera_resolution), colorfmt='rgb')
         self.texture_size = list(self.texture.size)
 
     def on_tex(self, *l):
+        super(AndroidCam, self).on_tex(*l)
         if self._camera._buffer is None:
             return None
         frame = self.frame_from_buf()
         self.frame_to_screen(frame)
-        super(AndroidCam, self).on_tex(*l)
+
 
     def frame_from_buf(self):
         w, h = self.resolution
         frame = np.frombuffer(self._camera._buffer.tostring(), 'uint8').reshape((h + h // 2, w))
-        frame_bgr = cv2.cvtColor(frame, 93)
-        return np.rot90(frame_bgr, 3)
+        #frame_bgr = cv2.cvtColor(frame, 93)
+        return frame
 
     def frame_to_screen(self, frame):
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        #cv2.putText(frame_rgb, str(self.counter), (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-        #self.counter += 1
         flipped = np.flip(frame_rgb, 0)
         buf = flipped.tostring()
         self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
@@ -55,6 +53,10 @@ class CamApp(MDApp):
         self.theme_cls.primary_palette = "Orange"
         Screen = Builder.load_file("GUI.kv")
         return Screen
-
+    def close_application(self):
+        # closing application
+        MDApp.get_running_app().stop()
+        # removing window
+        Window.close()
 if __name__ == '__main__':
     CamApp().run()
